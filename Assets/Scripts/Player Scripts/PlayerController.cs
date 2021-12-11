d79a70104125b604d0620a645c3056d23873180a
IbrahimHamwi <ibrahimhamwi_619@hotmail.com>
2021-12-12 02:11:44 +0300

    Adjusted Camera and Player rotation

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
+    private Rigidbody myBody;
     void Start()
     {
-
+        myBody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-
+        ControlMovementWithKeyboard();
+        ChangeRotation();
     }
+    void FixedUpdate()
+    {
+        MoveTank();
+    }
+    void MoveTank()
+    {
+        myBody.MovePosition(myBody.position + speed * Time.deltaTime);
+    }
+    void ControlMovementWithKeyboard()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            MoveFast();
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            MoveSlow();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            MoveStraight();
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            MoveStraight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            MoveNormal();
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            MoveNormal();
+        }
+    }
+
+    void ChangeRotation()
+    {
+        if (speed.x > 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+             Quaternion.Euler(0f, maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        }
+        else if (speed.x < 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+             Quaternion.Euler(0f, -maxAngle, 0f), Time.deltaTime * rotationSpeed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+            Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * rotationSpeed);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
